5ae1e2997d0b6272151ad9797a665c892d555d4d
Archina <archina5thrider@gmail.com>
2023-04-04 16:30:39 +0200

    use date-fns, allow calendar traversal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { addMonths, subMonths, getMonth, getYear } from 'date-fns';
     import Debug from "../components/Debug.svelte";
     import Entry from "../components/Entry.svelte";
     import Export from "../components/ImExport.svelte";
@@ -8,26 +9,64 @@
     const week_days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
     // Get the current year and month
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
+    let currentDate = addMonths(new Date(),1);
+    let currentYear = getYear(currentDate);
+    let currentMonth = getMonth(currentDate);
 
     // Calculate the number of days in the current month
-    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    let daysInMonth; let firstDay; let days: ({day: number, date: Date, identifier: [number, number, number]} | null)[];
+    init();
 
-    const firstDay = new Date(currentYear, currentMonth, 1).getDay();
+    function init(){
+        currentMonth = new Date(currentYear, currentMonth - 1.0).getMonth();
 
-    // Create an array of objects representing the days of the current month
-    const days = [...Array.from({length: firstDay}, _ => null),...Array.from({ length: daysInMonth }, (_, i) => ({
-        day: i + 1,
-        date: new Date(currentYear, currentMonth, i + 1),
-        identifier: [currentYear, currentMonth, i+1] as [number, number, number]
-    }))];
+        // Calculate the number of days in the current month
+        daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+        firstDay = new Date(currentYear, currentMonth, 1).getDay();
+
+        // Create an array of objects representing the days of the current month
+        days = [...Array.from({length: firstDay}, _ => null),...Array.from({ length: daysInMonth }, (_, i) => ({
+            day: i + 1,
+            date: new Date(currentYear, currentMonth, i + 1),
+            identifier: [currentYear, currentMonth, i+1] as [number, number, number]
+        }))];
+    }
+
+    function previous_month(){
+        currentDate = subMonths(currentDate, 1);
+        if(getMonth(currentDate) === 0){
+            currentMonth = getMonth(currentDate) + 12;
+            currentYear--;
+        } else {
+            currentMonth = getMonth(currentDate);
+            currentYear = getYear(currentDate);
+        }
+        init();
+    }
+
+    function next_month(){
+        currentDate = addMonths(currentDate, 1);
+        if(getMonth(currentDate) === 0){
+            currentMonth = getMonth(currentDate) - 12;
+            currentYear;
+        } else {
+            currentMonth = getMonth(currentDate);
+            currentYear = getYear(currentDate);
+        }
+        init();
+    }
 </script>
-<h1 class="cell">AlpTrack Calendar <Export /></h1>
+<div class="flex-row">
+    <h1 class="cell">AlpTrack Calendar</h1>
+    <Export />
+</div>
 <h2 class="cell text-smol">by Desdemona, 4 Earth-Girl</h2>
 <div class="calendar">
-    <div class="month-indicator cell">{months[currentMonth]} {currentYear}</div>
+    <div class="month-indicator cell">
+        <button on:click={previous_month}>&#x2039;</button>
+        <span class="month">{months[currentMonth]}</span>
+        <button on:click={next_month}>&#x203A;</button> {currentYear}</div>
     <div class="days-grid">
         {#each week_days as day}
         <div class="night cell">🌙</div>
@@ -49,6 +88,13 @@
         margin-bottom: 2rem;
     }
 
+    .flex-row{
+        display: flex;
+        flex-direction: row;
+        align-items: baseline;
+        column-gap: 4px;
+    }
+
     .cell{
         text-align: center;
         padding: 3px;
@@ -60,6 +106,11 @@
 
     .month-indicator{
         font-weight: bold;
+    }
+
+    .month{
+        width: 80px;
+        display: inline-block;
     }
 
     .day-indicator{
